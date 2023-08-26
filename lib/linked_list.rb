2716572a326f76b7323a542f5b8930000280d27e
Sooyung Kim <kimx1279@gmail.com>
2023-08-25 21:25:18 -0600

    Update count method

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -6,16 +6,23 @@ class LinkedList
 
   def append(sound)
     @head = Node.new(sound)
-   
+    # new_node = Node.new(sound)
+    #   if @head.nil?
+    #     @head = new_node
+    #   else
+    #     current = @head
+    #     current = current.next until current.next.nil?
+    #     current.next = new_node
+    #   end
   end
 
   def count
-    @count = 0
-    if head = nil
-      count = 0
-    else
-      @count += 1
-    end
+    @count =0
+    if @head == nil
+      return 0
+    elsif @head.next_node == nil
+      return @count += 1 
+    end 
   end
 
   def to_string
