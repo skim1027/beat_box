de0e7f65f1b4577021c7ac448a9fc931d76ea0c8
Sooyung Kim <kimx1279@gmail.com>
2023-08-29 12:03:06 -0600

    Update beat box class

--- lib/beat_box.rb
+++ lib/beat_box.rb
@@ -17,12 +17,8 @@ class BeatBox
   end
 
   def play
-<<<<<<< HEAD
     sound = @list.to_string
       `say -r 200 -v Boing "#{sound}"`
     sound
-=======
-    "say -r 500 Boing #{}"
->>>>>>> e91f54d673938fa37aefe8b800c9cf9e995fa2e5
   end
 end
